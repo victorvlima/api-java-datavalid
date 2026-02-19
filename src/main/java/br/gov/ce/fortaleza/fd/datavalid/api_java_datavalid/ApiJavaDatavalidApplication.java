1822d321c280e62e697e9f6977e85fd8dba212c8
Victor Vasconcelos <victor.vsc.lima @gmail.com>
2026-02-19 13:59:42 -0300

    Projeto base

---
+++ src/main/java/br/gov/ce/fortaleza/fd/datavalid/api_java_datavalid/ApiJavaDatavalidApplication.java
@@ -0,0 +1,13 @@
+package br.gov.ce.fortaleza.fd.datavalid.api_java_datavalid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJavaDatavalidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJavaDatavalidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/ce/fortaleza/fd/datavalid/api_java_datavalid/ApiJavaDatavalidApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.ce.fortaleza.fd.datavalid.api_java_datavalid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJavaDatavalidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
